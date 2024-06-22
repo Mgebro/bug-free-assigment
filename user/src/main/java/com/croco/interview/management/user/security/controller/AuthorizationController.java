ef1d26299d7dbdfc0132a6398e9c25f68037e502
Mgebro <thatdudewhostudies@gmail.com>
2024-06-23 01:35:22 +0400

    Authorization
     * fixed controller annotation

--- user/src/main/java/com/croco/interview/management/user/security/controller/AuthorizationController.java
+++ user/src/main/java/com/croco/interview/management/user/security/controller/AuthorizationController.java
@@ -6,13 +6,9 @@ import com.croco.interview.management.user.security.service.AuthorizationService
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/public/authorize")
 public class AuthorizationController {
@@ -24,5 +20,4 @@ public class AuthorizationController {
     AuthorizationResponse authorize(@RequestBody @Valid AuthorizationRequest request) {
         return authorizationService.authorize(request);
     }
-
 }
