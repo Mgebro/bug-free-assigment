de6bbc50a6e10115c72dcadbbb1e3533cda2a19e
Mgebro <thatdudewhostudies@gmail.com>
2024-06-23 17:02:17 +0400

    Security config
     * fix of security config because private methods were not allowed with tokens

--- order/src/main/java/com/croco/interview/management/order/SecurityConfig.java
+++ order/src/main/java/com/croco/interview/management/order/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**"
                         ).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .csrf(CsrfConfigurer::disable)
                 .cors(Customizer.withDefaults())
--- user/src/main/java/com/croco/interview/management/user/security/SecurityConfig.java
+++ user/src/main/java/com/croco/interview/management/user/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**"
                         ).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .csrf(CsrfConfigurer::disable)
                 .cors(Customizer.withDefaults())
