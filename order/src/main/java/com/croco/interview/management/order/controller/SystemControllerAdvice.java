9f714d1c1422af4b094d29574a3b5267ca29e538
Mgebro <thatdudewhostudies@gmail.com>
2024-06-23 23:24:07 +0400

    * Added missing controller advice for orders microservice

---
+++ order/src/main/java/com/croco/interview/management/order/controller/SystemControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.croco.interview.management.order.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+
+@ControllerAdvice
+public class SystemControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception exception) {
+        var map = new HashMap<String, String>();
+        map.put("message", exception.getLocalizedMessage());
+        return ResponseEntity.badRequest().body(map);
+    }
+}
