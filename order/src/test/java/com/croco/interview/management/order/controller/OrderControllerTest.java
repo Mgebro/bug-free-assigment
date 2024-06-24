738a54de48bc1df2a0b1da0110f4c8f5dacf55da
Mgebro <thatdudewhostudies@gmail.com>
2024-06-24 16:24:43 +0400

    Order service
     * Write tests for controller
     * Write tests for service

---
+++ order/src/test/java/com/croco/interview/management/order/controller/OrderControllerTest.java
@@ -0,0 +1,115 @@
+package com.croco.interview.management.order.controller;
+
+import com.croco.interview.management.order.model.enums.Status;
+import com.croco.interview.management.order.model.request.CreateOrderRequest;
+import com.croco.interview.management.order.model.request.UpdateOrderRequest;
+import com.croco.interview.management.order.model.response.OrderResponse;
+import com.croco.interview.management.order.model.response.OrdersResponse;
+import com.croco.interview.management.order.model.response.PageableResponse;
+import com.croco.interview.management.order.model.response.UserResponse;
+import com.croco.interview.management.order.service.OrderService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    @WithMockUser(username = "identifier")
+    void testCreateOrder() throws Exception {
+        String identifier = "identifier";
+        CreateOrderRequest request = new CreateOrderRequest(
+                "product1",
+                "2",
+                new BigDecimal(50));
+
+        mockMvc.perform(post("/api/order")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated());
+
+        verify(orderService, times(1)).createOrder(identifier, request);
+    }
+
+    @Test
+    @WithMockUser(username = "identifier")
+    void testGetOrder() throws Exception {
+        Long orderId = 1L;
+        OrderResponse response = new OrderResponse("product1", "2", new BigDecimal("50.0"), new UserResponse("user1"), Status.ACTIVE);
+        when(orderService.getOrder(orderId)).thenReturn(response);
+        mockMvc.perform(get("/api/order/{id}", orderId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.product").value("product1"));
+    }
+
+    @Test
+    @WithMockUser(username = "identifier")
+    void testGetOrders() throws Exception {
+        PageableResponse<OrdersResponse> pageableResponse = new PageableResponse<>(0, 20, 0L, List.of());
+
+        when(orderService.getOrders(0, 20, Optional.empty())).thenReturn(pageableResponse);
+
+        mockMvc.perform(get("/api/order/all")
+                        .param("page", "0")
+                        .param("size", "20"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isPartialContent());
+
+        verify(orderService, times(1)).getOrders(anyInt(), anyInt(), any(Optional.class));
+    }
+
+    @Test
+    @WithMockUser(username = "identifier")
+    void testUpdateOrder() throws Exception {
+        Long orderId = 1L;
+
+        UpdateOrderRequest request = new UpdateOrderRequest("product2", "3", new BigDecimal("60.0"));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/order/{id}", orderId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isNoContent());
+
+        verify(orderService, times(1)).updateOrder(orderId, request);
+    }
+
+    @Test
+    @WithMockUser(username = "identifier")
+    void testDeleteOrder() throws Exception {
+        Long orderId = 1L;
+
+        mockMvc.perform(delete("/api/order/{id}", orderId))
+                .andExpect(status().isNoContent());
+
+        verify(orderService).deleteOrder(orderId);
+    }
+}
+
---
+++ order/src/test/java/com/croco/interview/management/order/service/OrderServiceTest.java
@@ -0,0 +1,151 @@
+package com.croco.interview.management.order.service;
+
+import com.croco.interview.management.order.model.entity.Order;
+import com.croco.interview.management.order.model.entity.User;
+import com.croco.interview.management.order.model.enums.Status;
+import com.croco.interview.management.order.model.request.CreateOrderRequest;
+import com.croco.interview.management.order.model.request.UpdateOrderRequest;
+import com.croco.interview.management.order.model.response.OrderResponse;
+import com.croco.interview.management.order.model.response.OrdersResponse;
+import com.croco.interview.management.order.model.response.PageableResponse;
+import com.croco.interview.management.order.repository.OrderRepository;
+import com.croco.interview.management.order.repository.UserRepository;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+    @Captor
+    private ArgumentCaptor<Order> captor;
+
+    @NotNull
+    static User buildUser() {
+        return User.builder()
+                .withIdentifier("identifier")
+                .withId(1L)
+                .build();
+    }
+
+    @NotNull
+    private static Order buildOrder() {
+        return Order.builder()
+                .withId(1L)
+                .withProduct("product")
+                .withQuantity("2")
+                .withPrice(new BigDecimal("60.0"))
+                .withStatus(Status.ACTIVE)
+                .withUser(buildUser())
+                .build();
+    }
+
+    @Test
+    void testCreateOrder() {
+        User user = buildUser();
+
+        when(userRepository.findUserByIdentifier(user.getIdentifier())).thenReturn(user);
+
+        orderService.createOrder(user.getIdentifier(), new CreateOrderRequest("product", "2", new BigDecimal("50.0")));
+
+        verify(orderRepository).save(captor.capture());
+        Order order = captor.getValue();
+
+        assertEquals("product", order.getProduct());
+        assertEquals("2", order.getQuantity());
+        assertEquals(new BigDecimal("50.0"), order.getPrice());
+        assertEquals("identifier", order.getUser().getIdentifier());
+    }
+
+    @Test
+    void testGetOrder() {
+        Order order = buildOrder();
+        when(orderRepository.findOrderById(order.getId())).thenReturn(order);
+
+        OrderResponse response = orderService.getOrder(order.getId());
+
+        assertEquals("product", response.product());
+        assertEquals("2", response.quantity());
+        assertEquals(new BigDecimal("60.0"), response.product());
+        assertEquals("identifier", response.user().userIdentifier());
+    }
+
+    @Test
+    void testGetOrders() {
+        Page<Order> mockPage = mock(Page.class);
+
+        when(orderRepository.findAllByStatusIn(any(PageRequest.class), anyList())).thenReturn(mockPage);
+        when(mockPage.getPageable()).thenReturn(PageRequest.of(0, 20));
+        when(mockPage.getTotalElements()).thenReturn(0L);
+        when(mockPage.getContent()).thenReturn(List.of());
+
+        PageableResponse<OrdersResponse> response = orderService.getOrders(0, 20, Optional.of(List.of(Status.ACTIVE, Status.DELETED)));
+
+        assertNotNull(response);
+        assertEquals(0, response.page());
+        assertEquals(20, response.size());
+        assertEquals(0, response.total());
+        assertEquals(new ArrayList<>(), response.content());
+        verify(orderRepository).findAllByStatusIn(any(PageRequest.class), anyList());
+    }
+
+    @Test
+    void testUpdateOrder() {
+        Order order = buildOrder();
+
+        when(orderRepository.findOrderById(order.getId())).thenReturn(order);
+
+        orderService.updateOrder(order.getId(), new UpdateOrderRequest(
+                "product2",
+                "3",
+                new BigDecimal("70.0")
+        ));
+
+        verify(orderRepository).save(captor.capture());
+        Order capturedOrder = captor.getValue();
+
+        assertEquals("product2", order.getProduct());
+        assertEquals("3", order.getQuantity());
+        assertEquals(new BigDecimal("70.0"), order.getPrice());
+    }
+
+    @Test
+    void testDeleteOrder() {
+        Order order = buildOrder();
+
+        when(orderRepository.findOrderById(order.getId())).thenReturn(order);
+
+        orderService.deleteOrder(order.getId());
+
+        verify(orderRepository).save(captor.capture());
+        Order capturedOrder = captor.getValue();
+
+        assertEquals(Status.DELETED, capturedOrder.getStatus());
+    }
+}
+
